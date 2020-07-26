8acd1a1ba81ba0a73dda1725fe99ae235ed39708
Bebiezaza <Apipunnakul@gmail.com>
2020-07-26 13:44:19 +0700

    1.1.2_01 Alpha

--- commands/help.js
+++ commands/help.js
@@ -3,13 +3,21 @@ const prefix = process.env.PREFIX;
 module.exports = (client,message,embed) => {
     embed.setAuthor(client.user.username, client.user.avatarURL());
     embed.setColor('#f1c40f');
-    embed.setDescription(`command list:
+    embed.setDescription(`__**ALL COMMANDS**__\n
+    __**General**__
     **${prefix}help** = display commands
     **${prefix}hellobot** = response test
+    
+    __**Music**__
     **${prefix}play** / **${prefix}p** = play/queue songs
+    **${prefix}nowplaying** / **${prefix}np** = shows song that is playing in vc
+    **${prefix}queue** / **${prefix}q** = shows queue
     **${prefix}skip** / **${prefix}s**= skip a song
     **${prefix}disconnect** / **${prefix}dc** = disconnect
-    **${prefix}nowplaying** / **${prefix}np** = shows now playing song`);
+    
+    __**Utilities**__
+    **${prefix}nettest** = ping test
+    **${prefix}sourcerand** = source randomization, result not guaranteed`);
 
     return message.channel.send(embed);
 };
